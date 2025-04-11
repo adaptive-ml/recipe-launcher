5230905558617156fd8f3bc8c17acddb46a17e6f
João Moura <82108834+joaopcm1996@users.noreply.github.com>
2025-04-11 14:57:26 +0200

    Update docker-launch.sh

--- docker-launch.sh
+++ docker-launch.sh
@@ -1,16 +1,68 @@
+#!/bin/bash
+
+set -e;
+
 GPU_COUNT=$(nvidia-smi --list-gpus | wc -l)
+
+# check that recipe config exists
+if [ -z $local_recipe_file_json ]; then
+    echo "Recip config file envvar local_recipe_file_json was not set"
+    exit 1
+fi
+if [ -z $local_model_registry_path ]; then
+    echo "Model registry path envvar local_model_registry_path was not set"
+    exit 1
+fi
+if [ -z $data_dir ]; then
+    echo "Input data directory envvar data_dir was not set"
+    exit 1
+fi
+if [ -z $local_shared_directory ]; then
+    echo "Shared directory envvar local_shared_directory was not set"
+    exit 1
+fi
+if [ -z $adaptive_image_repo ]; then
+    echo "Image repo envvar adaptive_image_repo was not set"
+    exit 1
+fi
+if [ -z $adaptive_image_tag ]; then
+    echo "Image tag envvar adaptive_image_tag was not set"
+    exit 1
+fi
+
+# Define default flags
+DOCKER_RUN_FLAGS=""
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -d)
+            DOCKER_RUN_FLAGS+="-d "
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
 
 # before pulling make sure you have logged-in to the docker repo
 docker pull ${adaptive_image_repo}:${adaptive_image_tag}
 
-docker run --rm \
-  -d \
+# make directories if they do not exist
+mkdir -p ${local_model_registry_path} ${data_dir} ${local_shared_directory}
+sudo chmod -R 777 ${local_model_registry_path} ${training_data_dir} ${local_shared_directory}
+
+docker run ${DOCKER_RUN_FLAGS} \
+  --rm \
   --gpus all \
   --shm-size=8g \
   -v ${local_recipe_file_json}:/opt/adaptive/lib/adaptive/adaptive/recipe.json \
   -v ${local_model_registry_path}:/model_registry \
+  -v ${data_dir}:/data \
+  -v ${local_shared_directory}:/opt/adaptive/shared_folder \
   -e GPU_COUNT=${GPU_COUNT} \
-  -e WORLD_SIZE=1 \
+  -e WORLD_SIZE=${GPU_COUNT} \
   -e ADAPTIVE_LOGGING_LEVEL=INFO \
   -e HARMONY_SETTING_LOGGING_LEVEL=INFO \
   -e ADAPTIVE_MODE=PROD \
@@ -24,7 +76,4 @@ docker run --rm \
   ${adaptive_image_repo}:${adaptive_image_tag} 
 
 echo "Job was launched, check docker logs"
-
-
-
 
